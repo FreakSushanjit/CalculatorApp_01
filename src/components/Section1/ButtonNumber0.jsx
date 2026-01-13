547606275e9b7a0bead8e3159feb0e1bcb6778b8
SushanJit Bhattacherjee <sushanjit123@gmail.com>
2026-01-13 23:35:29 +0530

    8th Push

--- src/components/Section1/ButtonNumber0.jsx
+++ src/components/Section1/ButtonNumber0.jsx
@@ -8,7 +8,7 @@ const ButtonNumber = ({ number, setDisplay }) => {
   return (
     <div
       onClick={handleClick}
-      className="bg-gray-900 h-16 w-38 rounded-full text-amber-50 flex items-center justify-center m-2 hover:bg-gray-500 cursor-pointer text-2xl font-bold hover:scale-105 transition-all hover:shadow-lg"
+      className="bg-gray-900 h-16 w-34 rounded-full text-amber-50 flex items-center justify-center m-2 hover:bg-gray-500 cursor-pointer text-2xl font-bold hover:scale-105 transition-all hover:shadow-lg"
     >
       {number}
     </div>
--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -4,8 +4,7 @@ import Body from './Body'
 const Section1 = () => {
   return (
     <div className=" bg-blue-700 h-screen w-full flex items-center justify-center
-                
-                shadow-inner shadow-black/50 px-8 py-12">
+     shadow-inner shadow-black/50 px-8 py-12">
         <Body />
 
     </div>
